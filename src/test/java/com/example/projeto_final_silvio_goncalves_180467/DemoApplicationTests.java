18829e14f88b8f58528fe7a7bcae887c05a786fd
Saintt0 <58036125+Saintt0@users.noreply.github.com>
2020-05-26 21:27:14 -0300

    Att1

---
+++ src/test/java/com/example/projeto_final_silvio_goncalves_180467/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.projeto_final_silvio_goncalves_180467;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
