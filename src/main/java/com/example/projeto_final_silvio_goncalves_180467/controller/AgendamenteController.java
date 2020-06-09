0bc6d505b15dd7302aa91a2e3433848fc9f33c3b
Saintt0 <58036125+Saintt0@users.noreply.github.com>
2020-06-09 17:39:16 -0300

    Att17

--- src/main/java/com/example/projeto_final_silvio_goncalves_180467/controller/AgendamenteController.java
+++ src/main/java/com/example/projeto_final_silvio_goncalves_180467/controller/AgendamenteController.java
@@ -1,6 +1,9 @@
 package com.example.projeto_final_silvio_goncalves_180467.controller;
 
+import javax.servlet.http.HttpServletRequest;
+
 import com.example.projeto_final_silvio_goncalves_180467.entity.Agendamento;
+import com.example.projeto_final_silvio_goncalves_180467.entity.Servicos;
 import com.example.projeto_final_silvio_goncalves_180467.service.AgendamenteService;
 import com.example.projeto_final_silvio_goncalves_180467.service.CabelereiroService;
 import com.example.projeto_final_silvio_goncalves_180467.service.ClienteService;
@@ -12,6 +15,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
@@ -70,6 +74,30 @@ public class AgendamenteController {
         mv.addObject("clientes", cls.getTodoClientes());
         
         return mv;
+    }
+
+    @GetMapping("/escservicos/{id}")
+    public ModelAndView getServicos(@PathVariable (name = "id") Integer id)
+    {
+        ModelAndView mv = new ModelAndView("Agendamento_Servicos");
+        Agendamento a = as.getAgendamentoById(id);
+
+        mv.addObject("agendamento", a);
+        mv.addObject("servicos", a.getCabelereiro().getServicos());
+
+        return mv;
+    }
+
+    @PostMapping("/ligarservico")
+    public String ligarServicos(HttpServletRequest req, @RequestParam Integet id)
+    {
+        Agendamento a = as.getAgendamentoById(id);
+        Servicos s = ss.getServicoById(Integer.parseInt(req.getParameter("id")));
+
+        a.getServicos().add(s);
+        as.cadastrarAgendamento(a);
+
+        return "redirect:/escservicos/" + id;
     }
 
 
--- src/main/java/com/example/projeto_final_silvio_goncalves_180467/controller/CabelereiroController.java
+++ src/main/java/com/example/projeto_final_silvio_goncalves_180467/controller/CabelereiroController.java
@@ -1,7 +1,15 @@
 package com.example.projeto_final_silvio_goncalves_180467.controller;
 
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+
+import com.example.projeto_final_silvio_goncalves_180467.entity.Agendamento;
 import com.example.projeto_final_silvio_goncalves_180467.entity.Cabelereiro;
+import com.example.projeto_final_silvio_goncalves_180467.entity.Servicos;
+import com.example.projeto_final_silvio_goncalves_180467.service.AgendamenteService;
 import com.example.projeto_final_silvio_goncalves_180467.service.CabelereiroService;
+import com.example.projeto_final_silvio_goncalves_180467.service.ServicosService;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -9,6 +17,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
@@ -16,6 +25,12 @@ public class CabelereiroController {
     
     @Autowired
     private CabelereiroService cs;
+
+    @Autowired
+    private ServicosService ss;
+
+    @Autowired
+    private AgendamenteService as;
 
     @GetMapping("/cabelereiro")
     public ModelAndView getCabelereiro()
@@ -54,6 +69,48 @@ public class CabelereiroController {
         mv.addObject("cabelereiro", c);
         return mv;
     }
+
+    @GetMapping("/addservicos/{id}")
+    public ModelAndView getServicos(@PathVariable (name = "id") Integer id)
+    {
+        ModelAndView mv = new ModelAndView("Cabelereiro_Servicos");
+        Cabelereiro c = cs.getCabelereiroById(id);
+        List<Servicos> s = ss.getTodosServicos();
+
+        s.removeAll(c.getServicos());
+
+        mv.addObject(("cabelereiro"), c);
+        mv.addObject("servicos", s);
+
+        return mv;
+
+    }
+
+    @PostMapping("/addserv")
+    public String ligarServicos(HttpServletRequest req, @RequestParam Integer id)
+    {
+        Cabelereiro c = cs.getCabelereiroById(id);
+        Servicos s = ss.getServicoById(Integer.parseInt(req.getParameter("ids")));
+
+        c.getServicos().add(s);
+        cs.cadastrar(c);
+
+        return "redirect:/addservicos" + id;
+    }
+
+    @GetMapping("/mostraragendamentos/{id}")
+    public ModelAndView getAgendamentosCabelereiro(@PathVariable (name = "id") Integer id)
+    {
+        ModelAndView mv = new ModelAndView("Cabelereiro_Agendamento");
+        Cabelereiro c = cs.getCabelereiroById(id);
+        List<Agendamento> a = as.getTodosAgendamentos();
+
+        mv.addObject(("cabelereiro"), c);
+        mv.addObject(("agendamentos"), a);
+
+        return mv;
+    }
+
 
     
 }
