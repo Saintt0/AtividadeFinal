510a46a7a09a7b841483456003778b866a51e1ad
Saintt0 <58036125+Saintt0@users.noreply.github.com>
2020-06-09 12:48:47 -0300

    Att12

--- src/main/java/com/example/projeto_final_silvio_goncalves_180467/controller/CabelereiroController.java
+++ src/main/java/com/example/projeto_final_silvio_goncalves_180467/controller/CabelereiroController.java
@@ -3,6 +3,7 @@ package com.example.projeto_final_silvio_goncalves_180467.controller;
 import com.example.projeto_final_silvio_goncalves_180467.entity.Cabelereiro;
 import com.example.projeto_final_silvio_goncalves_180467.service.CabelereiroService;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -13,6 +14,7 @@ import org.springframework.web.servlet.ModelAndView;
 @Controller
 public class CabelereiroController {
     
+    @Autowired
     private CabelereiroService cs;
 
     @GetMapping("/cabelereiro")
--- src/main/java/com/example/projeto_final_silvio_goncalves_180467/service/ServicosService.java
+++ src/main/java/com/example/projeto_final_silvio_goncalves_180467/service/ServicosService.java
@@ -1,8 +1,32 @@
 package com.example.projeto_final_silvio_goncalves_180467.service;
 
+import java.util.List;
+
+import com.example.projeto_final_silvio_goncalves_180467.entity.Servicos;
+import com.example.projeto_final_silvio_goncalves_180467.repository.ServicosRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ServicosService {
-    
+
+    @Autowired
+    private ServicosRepository sr;
+
+    public List<Servicos> getTodosServicos(){
+        return sr.findAll();
+    }
+
+    public Servicos getServicoById(Integer id){
+        return sr.findById(id).get();
+    }
+
+    public void cadastrarServicos(Servicos s){
+        sr.save(s);
+    }
+
+    public void removerServicos(Servicos s){
+        sr.delete(s);
+    }
 }
