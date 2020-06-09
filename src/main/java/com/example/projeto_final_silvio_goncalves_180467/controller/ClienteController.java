47c2f709d5f4dc2fd26a89227fe2d38caa8825df
Saintt0 <58036125+Saintt0@users.noreply.github.com>
2020-06-09 09:36:43 -0300

    Att8, Cadastrando Clientes

---
+++ src/main/java/com/example/projeto_final_silvio_goncalves_180467/controller/ClienteController.java
@@ -0,0 +1,38 @@
+package com.example.projeto_final_silvio_goncalves_180467.controller;
+
+import com.example.projeto_final_silvio_goncalves_180467.entity.Cliente;
+import com.example.projeto_final_silvio_goncalves_180467.service.ClienteService;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class ClienteController {
+    
+    @Autowired
+    private ClienteService cs;
+
+    @GetMapping("/cliente")
+    public ModelAndView getClientes()
+    {
+        ModelAndView mv = new ModelAndView("ClienteTemplate");
+
+        mv.addObject("cliente", new Cliente());
+        mv.addObject("clientes", cs.getTodoClientes());
+
+        return mv;
+    }
+
+    @PostMapping("cadastrarcliente")
+    public String CadastrarCliente(@ModelAttribute Cliente c)
+    {
+        cs.cadastrar(c);
+
+        return "redirect:/cliente";
+    }
+
+}
